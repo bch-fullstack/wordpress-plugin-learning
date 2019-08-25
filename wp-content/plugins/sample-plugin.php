dbea60afb2f9e79f09979b06d7b91f9ae933a211
Tien Hoang Pham <tienhoang.pham@bc.fi>
2019-08-25 20:12:44 +0300

    Add plugin files and licencing info

---
+++ wp-content/plugins/sample-plugin.php
@@ -0,0 +1,24 @@
+<?php
+/*
+Plugin Name: Sample plugin
+Plugin URI: https://wordpress.org/plugins/sample-plugin/
+Description: sample plugin that does random things to help learning
+Version: 1.0.0
+Author: Helsinki Business College
+Author URI: http://www.bc.fi
+Text Domain: health-check
+Domain Path: /languages
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
