e19503313afc43df6b29d4c28a66b401418a9aef
Tien Hoang Pham <tienhoang.pham@bc.fi>
2019-08-25 20:19:20 +0300

    Add sample  must-used plugin

---
+++ wp-content/mu-plugins/sample-must-used-plugin.php
@@ -0,0 +1,24 @@
+<?php
+/*
+Plugin Name: Sample Must Use Plugin
+Plugin URI: https://wordpress.org/plugins/sample-must-use-plugin/
+Description: sample MUST-USE plugin that does random things to help learning. This plugin will always be activated and cannot be deactived accidentally by users. Must-used plugins are loaded before other plugins
+Version: 1.0.0
+Author: Helsinki Business College
+Author URI: http://www.bc.fi
+Text Domain: health-check
+Domain Path: /languages
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
